36b1ecfe9326f8d5b4d5b35cbc41e3cf711e5bc6
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-10-28 16:55:00 +0530

    Avg of array element and sum of array element

---
+++ AvgOfArrayElement.py
@@ -0,0 +1,7 @@
+#avg of element
+arr4 = [10,20,30]
+total = 0
+for i in arr4:
+    total += i
+avg = total/len(arr4)
+print(f"the avg of arr3 is :- {avg}")
---
+++ SumOfElementOfArray.py
@@ -0,0 +1,6 @@
+#Sum of elements
+arr3 = [10,20,30]
+sum = 0
+for i in arr3:
+    sum = sum + i
+print(f"the sum of arr3 is :- {sum}")
