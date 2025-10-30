5d62878af00a02c0a1a9d596d51dd7730db01764
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-10-30 17:44:32 +0530

    Check the number divisibility by only 5 and 11

--- check_num_divisible_by.py
+++ check_num_divisible_by.py
@@ -1,1 +1,10 @@
-!
+'''
+Check if a number is divisible by both 5 and 11.
+'''
+
+num = int(input("Enter a number: "))
+
+if num % 5 == 0 and num % 11 == 0:
+    print("The number is divisible by both 5 and 11.")
+else:
+    print("The number is not divisible by both 5 & 11.")
