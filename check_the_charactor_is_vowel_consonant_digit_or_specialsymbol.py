506ebec9ceefc582e3c59a204c4b6640c2e068ef
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-10-31 10:49:53 +0530

    check if a character is a vowel, consonant, digit, or special symbol.

--- check_the_charactor_is_vowel_consonant_digit_or_specialsymbol.py
+++ check_the_charactor_is_vowel_consonant_digit_or_specialsymbol.py
@@ -1,5 +1,16 @@
 '''
 Check if a character is a vowel, consonant, digit, or special symbol.
-
 '''
-character = input("Enter the character to check it is vowel, consonant, digit or special symbol :- ")
+character = input("Enter a character to check if it is vowel, consonant, digit, or special symbol: ")
+
+if character.isalpha():
+    if character.lower() in ['a', 'e', 'i', 'o', 'u']:
+        print("Vowel")
+    else:
+        print("Consonant")
+elif character.isdigit():
+    print("Digit")
+elif not character.isalnum():
+    print("Special Symbol")
+else:
+    print("Unknown Character Type")
