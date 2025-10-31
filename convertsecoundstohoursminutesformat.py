b40486342f0798d1356fd8d109d6dea0cabb3212
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-10-31 11:26:52 +0530

    convert seconds to hours, minutes, and seconds format

--- convertsecoundstohoursminutesformat.py
+++ convertsecoundstohoursminutesformat.py
@@ -1,9 +1,9 @@
 '''
-onvert seconds to hours, minutes, and seconds format
+convert seconds to hours, minutes, and seconds format
 '''
-seconds = int(input("Enter time in seconds: "))
+seconds = int(input("Enter time in seconds:"))
 hours = seconds // 3600
 seconds %= 3600
 minutes = seconds // 60
-seconds %= 60
+seconds %= 60 
 print(f"Time: {hours} hours, {minutes} minutes, {seconds} seconds")
