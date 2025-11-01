ae6ec871f5a67ac1fe41b1cab629b77ad939ffee
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-11-01 21:36:25 +0530

    Calculate the square and cube of a number.

--- calculatesquareandcubeofnumber.py
+++ calculatesquareandcubeofnumber.py
@@ -1,7 +1,8 @@
+'''
+Calculate the square and cube of a number.
+'''
 num = int(input("Enter the number:- "))
 square = num ** 2
 cube = num ** 3
-# print(f"square of number is :- {num} of {square}")
 print(f"Square of {num} is {square}")
 print(f"Cube of {num} is {cube}")
-# print(f"cube of number is :- {num} of {cube}" )
