4ba10ea1d56e8f854678bf6b3260d03c2fde78e9
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-11-02 17:16:50 +0530

    Check if a person is eligible for a driving license (age ≥ 18).

--- FindMaximumNumbersofArry.py
+++ FindMaximumNumbersofArry.py
@@ -1,5 +1,5 @@
 #Find Maximum Numbers of Array
-arr5 = [101,1,13,8,100,20,30]
+arr5 = [97,1,13,8,88,20,30]
 max = arr5[0]
 for i in arr5:
     if i > max:
---
+++ check_eligibility_for_driving_license.py
@@ -0,0 +1,10 @@
+'''
+Check if a person is eligible for a driving license (age ≥ 18).
+'''
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible for a driving license.")
+else:
+    print("You are not eligible for a driving license.")
