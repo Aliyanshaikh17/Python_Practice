3ad62fc444fd1e1d8066a528045f31dd7f3a911e
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-11-11 23:07:19 +0530

    Find length of string without using len()

--- find_length_of_string.py
+++ find_length_of_string.py
@@ -2,8 +2,8 @@
 Find length of string without using len()
 '''
 
-str1 = input("Enter the string :- ")
+string = input("Enter the string :- ")
 count = 0
-for i in str1:
+for i in string:
     count = count + 1
 print(f"the length of arr2 is :- {count}")
