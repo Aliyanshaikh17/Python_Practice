5d22675b33a1f3b25707ee46d3c22534860ee1a1
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-11-13 18:53:52 +0530

    Display multiplication tables from 10 to 15.

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,7 +1,15 @@
+# '''
+# Display multiplication tables from 1 to 10.
+# '''
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(f"{i} * {j} = {i*j}")
+#     print("==================")
+
 '''
-Display multiplication tables from 1 to 10.
+Display multiplication tables from 10 to 15.
 '''
-for i in range(1,11):
+for i in range(1,16):
     for j in range(1,11):
         print(f"{i} * {j} = {i*j}")
     print("==================")
