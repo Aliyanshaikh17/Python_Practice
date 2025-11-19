d63e7fe3964a6de317c7067045a1a8dd38a738e1
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-11-19 10:38:50 +0530

    find the area of house

---
+++ find_area_of_house.py
@@ -0,0 +1,10 @@
+'''
+find the area of house
+'''
+
+
+hight = float(input("Enter the hight of house :- "))
+length = float(input("Enter the length of house :- "))
+Area = hight * length
+
+print(f"Area of house is :- {Area}")
