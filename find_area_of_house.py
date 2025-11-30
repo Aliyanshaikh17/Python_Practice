38722421f50957d7ac2fe2ffa77d555bfc86f304
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-11-30 15:21:50 +0530

    i tray to solve this example in 2 way with and without using functions

--- find_area_of_house.py
+++ find_area_of_house.py
@@ -2,9 +2,27 @@
 find the area of house
 '''
 
+''' without using functions '''
 
 hight = float(input("Enter the hight of house :- "))
 length = float(input("Enter the length of house :- "))
 Area = hight * length
 
 print(f"Area of house is :- {Area}")
+
+
+
+
+print("----------------------------------------------------------")
+
+
+
+''' with using functions '''
+
+def house_area(height, length):
+    return height * length
+
+h = float(input("Enter the height of house: "))
+l = float(input("Enter the length of house: "))
+
+print(f"Area of house is: {house_area(h, l)}")
