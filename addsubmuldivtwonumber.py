81870e1b57d39feab1630693759a3915538d6282
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-01 10:07:22 +0530

    i try to solve this que in another way

--- addsubmuldivtwonumber.py
+++ addsubmuldivtwonumber.py
@@ -1,20 +1,33 @@
-#Add of 2 Number
-num1 = int(input("Enter the first number:- "))
-num2 = int(input("Enter the second number :- "))
-print("Addition of num1 and num2 is :- ",num1 + num2)
+# #Add of 2 Number
+# num1 = int(input("Enter the first number:- "))
+# num2 = int(input("Enter the second number :- "))
+# print("Addition of num1 and num2 is :- ",num1 + num2)
 
-#Sub of 2 Number
-num1 = int(input("Enter the first number:- "))
-num2 = int(input("Enter the second number :- "))
-print("subtraction of num1 and num2 is :- ",num1 - num2)
+# #Sub of 2 Number
+# num1 = int(input("Enter the first number:- "))
+# num2 = int(input("Enter the second number :- "))
+# print("subtraction of num1 and num2 is :- ",num1 - num2)
 
-#multiply 2 Number
-num1 = int(input("Enter the first number:- "))
-num2 = int(input("Enter the second number :- "))
-print("multiplication of num1 and num2 is :- ",num1 * num2)
+# #multiply 2 Number
+# num1 = int(input("Enter the first number:- "))
+# num2 = int(input("Enter the second number :- "))
+# print("multiplication of num1 and num2 is :- ",num1 * num2)
 
 
-#divide 2 Number
-num1 = int(input("Enter the first number:- "))
-num2 = int(input("Enter the second number :- "))
-print("divide of num1 and num2 is :- ",num1 / num2)
+# #divide 2 Number
+# num1 = int(input("Enter the first number:- "))
+# num2 = int(input("Enter the second number :- "))
+# print("divide of num1 and num2 is :- ",num1 / num2)
+
+
+
+''' Simple calculator for two numbers 
+Addition , Subtraction , Multiplication ,Division  '''
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Addition =", num1 + num2)
+print("Subtraction =", num1 - num2)
+print("Multiplication =", num1 * num2)
+print("Division =", num1 / num2)
