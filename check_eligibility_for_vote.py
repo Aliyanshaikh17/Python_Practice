18a1b4dbae39bfacf1ff6de0ef606280a7927e12
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-03 22:31:17 +0530

    i try to solve this que in another way

--- check_eligibility_for_vote.py
+++ check_eligibility_for_vote.py
@@ -7,3 +7,15 @@ if Age >= 18:
     print("You are eligible to vote.")
 else:
     print("You are not eligible to vote.")    
+
+
+
+''' method 2 '''
+
+
+'''
+Check eligibility for voting
+'''
+age = int(input("Enter your age: "))
+
+print("You are eligible to vote.") if age >= 18 else print("You are not eligible to vote.")
