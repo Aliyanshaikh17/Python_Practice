0643427e72a39c07441a84bb9c4ac6215813817a
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-09 21:38:27 +0530

    swap to numbers

---
+++ swap_two_numbers.py
@@ -0,0 +1,11 @@
+'''
+Swap two numbers 
+'''
+
+num1 = int(input("Enter first number:- "))
+num2 = int(input("Enter second number:- "))
+num1, num2 = num2, num1
+
+print("After swapping:")
+print("first number is:-", num1)
+print("second number is :- ", num2)
