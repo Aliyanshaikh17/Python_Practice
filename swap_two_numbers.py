8f8f1fc12bacf74b7b1830e7d51fafb32ffcd023
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-10 22:07:17 +0530

    i try to solve this example in another way

--- swap_two_numbers.py
+++ swap_two_numbers.py
@@ -9,3 +9,22 @@ num1, num2 = num2, num1
 print("After swapping:")
 print("first number is:-", num1)
 print("second number is :- ", num2)
+
+
+
+
+'''
+i try to solve this example in another way
+'''
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
+
