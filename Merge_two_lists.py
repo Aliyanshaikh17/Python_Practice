4af774e74c45254996c56339d01567df15f7dace
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-13 19:39:32 +0530

    I tried solving it using user input.

--- Merge_two_lists.py
+++ Merge_two_lists.py
@@ -7,3 +7,14 @@ list2 = [4, 5, 6]
 
 merged = list1 + list2
 print(merged)
+
+
+'''
+I tried solving it using user input.
+'''
+
+list1 = input("Enter first list items: ").split()
+list2 = input("Enter second list items: ").split()
+
+merged = list1 + list2
+print("Merged List:", merged)
