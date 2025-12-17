772474d5a7db5c3c09e460004d7b03d241ee5d63
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-17 22:46:53 +0530

    Print star pattern using user input

--- Print_star_pattern.py
+++ Print_star_pattern.py
@@ -5,3 +5,11 @@ Print star pattern
 for i in range(1, 4):
     print("*" * i)
 
+'''
+Print star pattern using user input
+'''
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+    print("*" * i)
