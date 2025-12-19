f5b2bf90a353ee5c41c8aeb2c9cefe5ed79dc74d
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-19 19:12:30 +0530

    # i try to solve this example using while loop

--- number_in_reverse.py
+++ number_in_reverse.py
@@ -5,3 +5,14 @@ Display numbers in reverse order (n to 1).
 num = int(input("Enter the number :- "))
 for i in range(num,0,-1):
     print(i)
+
+
+
+# i try to solve this example using while loop
+
+
+num = int(input("Enter the number: "))
+
+while num >= 1:
+    print(num)
+    num -= 1
