bc6741f2db7ec8518efe25dfa46c477cec27d569
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-20 19:13:32 +0530

    #Find length of array with using len()

--- FindLengthOfArray.py
+++ FindLengthOfArray.py
@@ -4,3 +4,10 @@ count = 0
 for i in arr2:
     count = count + 1
 print(f"the length of arr2 is :- {count}")
+
+
+
+#Find length of array with using len()
+
+arr = [10,20,30]
+print("Length of array:", len(arr))
