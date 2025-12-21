28640c730a4c59222d1f0d462b418cadbaf0a7d4
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-21 22:15:10 +0530

    i try to solve this example another way Using (One-line if-else)

--- check_eligibility_for_driving_license.py
+++ check_eligibility_for_driving_license.py
@@ -8,3 +8,12 @@ if age >= 18:
     print("You are eligible for a driving license.")
 else:
     print("You are not eligible for a driving license.")
+
+
+''' i try to solve this example another way 
+Using (One-line if-else)
+'''
+
+age = int(input("Enter your age: "))
+
+print("You are eligible for a driving license." if age >= 18 else "You are not eligible for a driving license.")
