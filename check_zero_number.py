4ba1d6ee632a37774a6b31dc8ed32f567c230b22
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-22 15:00:14 +0530

    Write a program to check whether a given number is zero or not.

---
+++ check_zero_number.py
@@ -0,0 +1,10 @@
+'''
+Write a program to check whether a given number is zero or not.
+
+'''
+
+marks = int(input("Enter your marks:- "))
+if marks == 0:
+    print("The number is zero ")
+else:
+    print("The number is not zero")
