b76a8b840033e7bfae4c3c0fd8c9115df38ce305
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-24 13:06:38 +0530

    Write a program to check pass or fail based on marks.

---
+++ student_result_check.py
@@ -0,0 +1,9 @@
+'''
+Write a program to check pass or fail based on marks.
+'''
+
+marks = int(input("Enter your marks:- "))
+if marks >= 35:
+    print("You are pass in exam")
+else:
+    print("You are fail in exam")
