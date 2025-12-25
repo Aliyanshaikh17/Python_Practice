e1c048a42b8b0a8a51c0ecf6525d959568d707cc
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-25 13:35:47 +0530

    Write a program to check whether a student is eligible for scholarship

---
+++ student_is_eligible_for_scholarship.py
@@ -0,0 +1,11 @@
+'''
+Write a program to check whether a student is eligible for scholarship
+'''
+
+annual_income = float(input("Enter your father annual income:- "))
+
+if annual_income >= 800000:
+    print("You are not eligible for scholarship")
+else:
+    print("You are eligible for scholarship")
+    
