ee441091400416a7171654f6de086696a7db6124
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2025-12-30 08:48:11 +0530

    i solve this example using user input

--- print_square_of_num_from_1_to_5.py
+++ print_square_of_num_from_1_to_5.py
@@ -4,3 +4,11 @@ Write a program to print the square of numbers from 1 to 5 using a for loop
 
 for i in range(1, 6):
     print(i * i)
+
+
+#using user input
+
+n = int(input("Enter the last number: "))
+
+for i in range(1, n + 1):
+    print(i * i)
