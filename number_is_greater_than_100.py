680c40a5b1f08906bcc2a46548be23a9f315e70a
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-03 18:51:41 +0530

    Write a program to check whether a number is greater than 100

---
+++ number_is_greater_than_100.py
@@ -0,0 +1,10 @@
+'''
+Write a program to check whether a number is greater than 100
+'''
+
+num = int(input("Enter the number:- "))
+
+if num > 100:
+    print("number is greter than 100")
+else:
+    print("number is not greter than zero")
