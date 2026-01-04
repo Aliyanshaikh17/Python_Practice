2f74762a949d4ae3a345cba09179d0b43b25e89b
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-04 22:05:34 +0530

    Write a program to check whether a given number lies between 10 and 50

---
+++ number_lies_between_10_and_50.py
@@ -0,0 +1,10 @@
+'''
+Write a program to check whether a given number lies between 10 and 50
+'''
+
+num = int(input("Enter the number:- "))
+
+if 10 < num < 50:
+    print("Number line is between 10 and 50")
+else:
+    print("Number is not between 10 to 50")
