052432c4e266b09c28bc3e4fe530c428fa4bfd09
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-05 09:24:52 +0530

    Write a program to check whether an employee is eligible for promotion

---
+++ check_an_employee_eligible_for_promotion.py
@@ -0,0 +1,10 @@
+'''
+Write a program to check whether an employee is eligible for promotion
+'''
+
+work_ex = float(input("Enter your work experience  in this company:- "))
+
+if work_ex >= 6:
+    print("you are eligible for promotion ")
+else:
+    print("you are not eligible for promotion ")
