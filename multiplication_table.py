47a7428c1d4a255b3647e8847c781d091160ffc5
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-07 15:46:10 +0530

    Display multiplication tables from 20 to 32.

--- multiplication_table.py
+++ multiplication_table.py
@@ -13,3 +13,13 @@ for i in range(1,16):
     for j in range(1,11):
         print(f"{i} * {j} = {i*j}")
     print("==================")
+
+
+'''
+Display multiplication tables from 20 to 32.
+'''
+
+for i in range(20, 33):
+    for j in range(1, 11):
+        print(f"{i} * {j} = {i * j}")
+    print("==================")
