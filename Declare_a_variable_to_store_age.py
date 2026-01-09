a7735fee4ea7b304429b32ce9161f61ce6a69656
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-01-09 22:34:30 +0530

    # Checking age category

--- Declare_a_variable_to_store_age.py
+++ Declare_a_variable_to_store_age.py
@@ -10,3 +10,12 @@ print(age)
 
 age = int(input("Enter your age:- "))
 print(age)
+
+
+# Checking age category
+if age < 18:
+    print("You are a minor.")
+elif age < 60:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
